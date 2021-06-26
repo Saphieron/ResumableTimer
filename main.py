84406b37096a088e3784debe52679d10d270e389
Cichiwskyj <christopher.cichiwskyj@uni-due.de>
2021-06-26 21:43:11 +0200

    Timer can be started, halted and resumed by using the left mouse click. Timer can be reset, i.e. stopped back to 0 by right mouse click.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import time
 
 precision = 100
 
-STATE_RESET = 'STATE_RESET'
+STATE_STOPPED = 'STATE_STOPPED'
 STATE_RUNNING = 'STATE_RUNNING'
 STATE_HALTED = 'STATE_HALTED'
+
 
 def get_time_string(time_passed):
     all_seconds = int(time_passed)
@@ -27,55 +28,61 @@ def get_time_string(time_passed):
 class ReusableTimer:
 
     def __init__(self):
-        self.window = Tk()
+        self.window = None
         self.time_label = None
         self.date_label = None
+        self.label_frame = None
 
         self.total_time = 0
         self.previous_time = 0
 
-        self.running = False
-        self.time_label_after_obj = None
-        self.is_timer_running = False
-
-        self.app_state = "reset"
+        self.time_label_tki_after_obj = None
+        self.timer_state = STATE_STOPPED
+        # self.is_timer_running = False
 
     def clicked_label(self):
-        if not self.running:
-            self.start_timer()
-        else:
-            self.stop_timer()
+        pass
+        # if not self.running:
+        #     self.start_timer()
+        # else:
+        #     self.stop_timer()
 
     def make_window(self):
+        self.window = Tk()
+        self.window.configure(bg='#371C6B')
         self.window.title("Timer")
         self.window.geometry('400x150')
-        self.time_label = Label(self.window, font='calibri 40 bold', foreground='black')
-        self.time_label.pack(anchor='center')
+        self.window.bind("<Button-1>", self.click_left_time_label)
+        self.window.bind("<Button-3>", self.click_right_time_label)
 
-        # Button(self.window, text="Start Scan", command=self.start)
-        # Button(self.window, text="Stop", command=self.stop)
-
-        self.time_label.bind("<Button-1>", self.click_left_time_label)
-        self.time_label.bind("<Button-3>", self.click_right_time_label)
+        self.time_label = Label(self.window, font='Terminal 30 normal', foreground='#371C6B',
+                                text=get_time_string(self.total_time),
+                                anchor='e')
+        self.time_label.pack(fill=X, anchor='center')
+        self.time_label.configure(background='#371C6B', foreground='#af164e')
 
     def start_timer(self):
         self.total_time = 0
-        self.previous_time = time.perf_counter()
-        self.is_timer_running = True
-        self.update_clock()
-
-    def restart_timer(self):
-        self.previous_time = time.perf_counter()
-        self.is_timer_running = True
-        self.update_clock()
+        self.resume_timer()
 
     def stop_timer(self):
-        print("doing anything after timer should be stopped")
-        self.time_label.after_cancel(self.time_label_after_obj)
-        self.is_timer_running = False
+        self.total_time = 0
+        self.time_label.after_cancel(self.time_label_tki_after_obj)
+        time_string = get_time_string(self.total_time)
+        self.time_label.config(text=time_string)
+        self.timer_state = STATE_STOPPED
 
+    def halt_timer(self):
+        self.update_timer_label()
+        self.time_label.after_cancel(self.time_label_tki_after_obj)
+        self.timer_state = STATE_HALTED
 
-    def update_clock(self):
+    def resume_timer(self):
+        self.previous_time = time.perf_counter()
+        self.update_timer()
+        self.timer_state = STATE_RUNNING
+
+    def update_timer_label(self):
         current = time.perf_counter()
         diff = current - self.previous_time
         self.previous_time = current
@@ -83,25 +90,27 @@ class ReusableTimer:
         time_string = get_time_string(self.total_time)
         self.time_label.config(text=time_string)
 
-        # def callback(event):
-        #     if True:
-        #         print("clicked2")
-        #         # 'solve' is used here to stop the after... methods.
-        #         self.time_label.after_cancel(solve)
-        #
-        # self.time_label.bind("<Button-1>", callback)
-        self.time_label_after_obj = self.time_label.after(10, self.update_clock)
+    def update_timer(self):
+        self.update_timer_label()
+        self.time_label_tki_after_obj = self.time_label.after(10, self.update_timer)
 
     def click_left_time_label(self, event):
-        print("time label clicked left with event {}".format(event))
-        if self.is_timer_running:
-            self.stop_timer()
-        else:
-            if self.time
+        print("window clicked left with event {}".format(event))
+        if self.timer_state == STATE_RUNNING:
+            self.halt_timer()
+            print("halted")
+            return
+        elif self.timer_state == STATE_HALTED:
+            self.resume_timer()
+            print("resumed")
+            return
+        elif self.timer_state == STATE_STOPPED:
             self.start_timer()
+            print("started")
+            return
 
     def click_right_time_label(self, event):
-        print("time label clicked right with event {}".format(event))
+        self.stop_timer()
 
     def run_mainloop(self):
         self.window.mainloop()
@@ -110,205 +119,8 @@ class ReusableTimer:
 def main():
     timer = ReusableTimer()
     timer.make_window()
-    timer.start_timer()
     timer.run_mainloop()
 
 
 if __name__ == "__main__":
     main()
-
-# def clock():
-#     date_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S/%p")
-#     date, time1 = date_time.split()get_time_string()
-#     time2, time3 = time1.split('/')
-#     hour, minutes, seconds = time2.split(':')
-#     if 11 < int(hour) < 24:
-#         time = str(int(hour) - 12) + ':' + minutes + ':' + seconds + ' ' + time3
-#     else:
-#         time = time2 + ' ' + time3
-#     time_label.config(text=time)
-#     date_label.config(text=date)
-#     time_label.after(1000, clock)
-
-
-# window = Tk()
-# make_window(window)
-# clock()
-# window.mainloop()
-
-#
-#
-# def alarm():
-#     main_time = datetime.datetime.now().strftime("%H:%M %p")
-#     alarm_time = get_alarm_time_entry.get()
-#     alarm_time1, alarm_time2 = alarm_time.split(' ')
-#     alarm_hour, alarm_minutes = alarm_time1.split(':')
-#     main_time1, main_time2 = main_time.split(' ')
-#     main_hour1, main_minutes = main_time1.split(':')
-#     if main_time2 == 'PM':
-#         main_hour = str(int(main_hour1) - 12)
-#     else:
-#         main_hour = main_hour1
-#     if int(alarm_hour) == int(main_hour) and int(alarm_minutes) == int(main_minutes) and main_time2 == alarm_time2:
-#         for i in range(3):
-#             alarm_status_label.config(text='Time Is Up')
-#             if platform.system() == 'Windows':
-#                 winsound.Beep(5000, 1000)
-#             elif platform.system() == 'Darwin':
-#                 os.system('say Time is Up')
-#             elif platform.system() == 'Linux':
-#                 os.system('beep -f 5000')
-#         get_alarm_time_entry.config(state='enabled')
-#         set_alarm_button.config(state='enabled')
-#         get_alarm_time_entry.delete(0, END)
-#         alarm_status_label.config(text='')
-#     else:
-#         alarm_status_label.config(text='Alarm Has Started')
-#         get_alarm_time_entry.config(state='disabled')
-#         set_alarm_button.config(state='disabled')
-#     alarm_status_label.after(1000, alarm)
-#
-#
-# def stopwatch_counter(label):
-#     def count():
-#         if stopwatch_running:
-#             global stopwatch_counter_num
-#             if stopwatch_counter_num == 66600:
-#                 display = "Starting..."
-#             else:
-#                 tt = datetime.datetime.fromtimestamp(stopwatch_counter_num)
-#                 string = tt.strftime("%H:%M:%S")
-#                 display = string
-#             label.config(text=display)
-#             label.after(1000, count)
-#             stopwatch_counter_num += 1
-#
-#     count()
-#
-#
-# def stopwatch(work):
-#     if work == 'start':
-#         global stopwatch_running
-#         stopwatch_running = True
-#         stopwatch_start.config(state='disabled')
-#         stopwatch_stop.config(state='enabled')
-#         stopwatch_reset.config(state='enabled')
-#         stopwatch_counter(stopwatch_label)
-#     elif work == 'stop':
-#         stopwatch_running = False
-#         stopwatch_start.config(state='enabled')
-#         stopwatch_stop.config(state='disabled')
-#         stopwatch_reset.config(state='enabled')
-#     elif work == 'reset':
-#         global stopwatch_counter_num
-#         stopwatch_running = False
-#         stopwatch_counter_num = 66600
-#         stopwatch_label.config(text='Stopwatch')
-#         stopwatch_start.config(state='enabled')
-#         stopwatch_stop.config(state='disabled')
-#         stopwatch_reset.config(state='disabled')
-#
-#
-# def timer_counter(label):
-#     def count():
-#         global timer_running
-#         if timer_running:
-#             global timer_counter_num
-#             if timer_counter_num == 66600:
-#                 for i in range(3):
-#                     display = "Time Is Up"
-#                     if platform.system() == 'Windows':
-#                         winsound.Beep(5000, 1000)
-#                     elif platform.system() == 'Darwin':
-#                         os.system('say Time is Up')
-#                     elif platform.system() == 'Linux':
-#                         os.system('beep -f 5000')
-#                 timer_running = False
-#                 timer('reset')
-#             else:
-#                 tt = datetime.datetime.fromtimestamp(timer_counter_num)
-#                 string = tt.strftime("%H:%M:%S")
-#                 display = string
-#                 timer_counter_num -= 1
-#             label.config(text=display)
-#             label.after(1000, count)
-#
-#     count()
-#
-#
-# def timer(work):
-#     if work == 'start':
-#         global timer_running, timer_counter_num
-#         timer_running = True
-#         if timer_counter_num == 66600:
-#             timer_time_str = timer_get_entry.get()
-#             hours, minutes, seconds = timer_time_str.split(':')
-#             minutes = int(minutes) + (int(hours) * 60)
-#             seconds = int(seconds) + (minutes * 60)
-#             timer_counter_num = timer_counter_num + seconds
-#         timer_counter(timer_label)
-#         timer_start.config(state='disabled')
-#         timer_stop.config(state='enabled')
-#         timer_reset.config(state='enabled')
-#         timer_get_entry.delete(0, END)
-#     elif work == 'stop':
-#         timer_running = False
-#         timer_start.config(state='enabled')
-#         timer_stop.config(state='disabled')
-#         timer_reset.config(state='enabled')
-#     elif work == 'reset':
-#         timer_running = False
-#         timer_counter_num = 66600
-#         timer_start.config(state='enabled')
-#         timer_stop.config(state='disabled')
-#         timer_reset.config(state='disabled')
-#         timer_get_entry.config(state='enabled')
-#         timer_label.config(text='Timer')
-#
-#
-# tabs_control = Notebook(window)
-# clock_tab = Frame(tabs_control)
-# alarm_tab = Frame(tabs_control)
-# stopwatch_tab = Frame(tabs_control)
-# timer_tab = Frame(tabs_control)
-#
-# tabs_control.add(clock_tab, text="Clock")
-# tabs_control.add(alarm_tab, text="Alarm")
-# tabs_control.add(stopwatch_tab, text='Stopwatch')
-# tabs_control.add(timer_tab, text='Timer')
-# tabs_control.pack(expand=1, fill="both")
-#
-# time_label = Label(clock_tab, font='calibri 40 bold', foreground='black')
-# time_label.pack(anchor='center')
-# date_label = Label(clock_tab, font='calibri 40 bold', foreground='black')
-# date_label.pack(anchor='s')
-# get_alarm_time_entry = Entry(alarm_tab, font='calibri 15 bold')
-# get_alarm_time_entry.pack(anchor='center')
-# alarm_instructions_label = Label(alarm_tab, font='calibri 10 bold',
-#                                  text="Enter Alarm Time. Eg -> 01:30 PM, 01 -> Hour, 30 -> Minutes")
-# alarm_instructions_label.pack(anchor='s')
-# set_alarm_button = Button(alarm_tab, text="Set Alarm", command=alarm)
-# set_alarm_button.pack(anchor='s')
-# alarm_status_label = Label(alarm_tab, font='calibri 15 bold')
-# alarm_status_label.pack(anchor='s')
-# stopwatch_label = Label(stopwatch_tab, font='calibri 40 bold', text='Stopwatch')
-# stopwatch_label.pack(anchor='center')
-# stopwatch_start = Button(stopwatch_tab, text='Start', command=lambda: stopwatch('start'))
-# stopwatch_start.pack(anchor='center')
-# stopwatch_stop = Button(stopwatch_tab, text='Stop', state='disabled', command=lambda: stopwatch('stop'))
-# stopwatch_stop.pack(anchor='center')
-# stopwatch_reset = Button(stopwatch_tab, text='Reset', state='disabled', command=lambda: stopwatch('reset'))
-# stopwatch_reset.pack(anchor='center')
-# timer_get_entry = Entry(timer_tab, font='calibiri 15 bold')
-# timer_get_entry.pack(anchor='center')
-# timer_instructions_label = Label(timer_tab, font='calibri 10 bold',
-#                                  text="Enter Timer Time. Eg -> 01:30:30, 01 -> Hour, 30 -> Minutes, 30 -> Seconds")
-# timer_instructions_label.pack(anchor='s')
-# timer_label = Label(timer_tab, font='calibri 40 bold', text='Timer')
-# timer_label.pack(anchor='center')
-# timer_start = Button(timer_tab, text='Start', command=lambda: timer('start'))
-# timer_start.pack(anchor='center')
-# timer_stop = Button(timer_tab, text='Stop', state='disabled', command=lambda: timer('stop'))
-# timer_stop.pack(anchor='center')
-# timer_reset = Button(timer_tab, text='Reset', state='disabled', command=lambda: timer('reset'))
-# timer_reset.pack(anchor='center')
